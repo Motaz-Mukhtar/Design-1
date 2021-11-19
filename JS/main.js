1cb4bd88fecb95af76ec2cc097c00399994dc382
Motaz-Mukhtar <motazksa2@gmail.com>
2021-11-19 12:56:41 +0300

    Add a Scroll Up Button

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,30 @@
 document.forms[1].onsubmit = (e) => {
     e.preventDefault();
 };
+
+
+let scroll = document.querySelector(".scroll");
+
+console.log(scroll);
+
+
+
+scroll.onclick = function() {
+    window.scrollTo(0, 0)
+}
+
+if(window.pageYOffset === 0) {
+    scroll.style.cssText = "display: none; cursor: auto;"
+}
+
+window.onscroll = function() {
+    if (window.pageYOffset <= 500) {
+        scroll.style.cssText = "opacity: 0; cursor: auto;";
+    }
+    else {
+        scroll.style.cssText = "opacity: 1; background-color: #0ac6fc;";
+    }
+}
+
+
+
